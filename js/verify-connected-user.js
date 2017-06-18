dfb0a2c937eeccf3688aa0a0040cc683b646f19f
Cristian Cojocaru <cristiancojocaru@Cristians-Mac-mini.local>
2017-06-18 22:03:29 +0300

    login flow

--- js/verify-connected-user.js
+++ js/verify-connected-user.js
@@ -3,10 +3,20 @@ function verifyIfConnected(callback) {
 	firebase.auth().onAuthStateChanged(function(user) {
 		if (user) {
 			currentUser = user;
+			
 			callback(user);
 		} else {
-			console.log('nelogat')// No user is signed in.
+			console.log('nelogat');// No user is signed in.
+			window.location = '../login.html';
 		}
 	})
+}
+
+function logoutUser() {
+	firebase.auth().signOut().then(function() {
+  // Sign-out successful.
+}).catch(function(error) {
+  // An error happened.
+});
 }
 
