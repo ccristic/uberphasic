db6355ea49ce54b6b55d85a82c4dd887b964953e
Cristi <cristian.cojocaru@maxcode.net>
2017-06-20 16:07:53 +0300

    display my schedule, crud table

---
+++ js/schedules-schedules.js
@@ -0,0 +1,106 @@
+function getChartDataFromSchedule(schedule) {
+    var hours = [], colors = [], labels = [];
+    for (var i = 0; i < schedule.naps.length; i++) {
+        hours.push(((schedule.naps[i].stop - schedule.naps[i].start)/60).toFixed(2));
+
+        if(hours[hours.length-1] <= 1) {
+            labels.push(' Nap');
+            colors.push('#ff3f68'); 
+        }
+        else {
+            labels.push(' Core');
+            colors.push('#ffce0a');
+        }
+
+        if (i< schedule.naps.length - 1) {
+            hours.push(((schedule.naps[i+1].start - schedule.naps[i].stop)/60).toFixed(2)); 
+            colors.push('#3b3c5b');
+            labels.push(' Awake');
+        }
+        else { 
+            hours.push(((1440 - schedule.naps[i].stop)/60).toFixed(2)); 
+            colors.push('#3b3c5b'); 
+            labels.push(' Awake');   
+        }
+    }
+    return {
+        type: 'pie',
+        options: {
+            legend: {
+                display: false
+            }
+        },
+        data: {
+            datasets: [{
+                data: hours,
+                backgroundColor: colors,                        
+                borderWidth: 0
+            }],
+            labels: labels
+        }
+    };
+}
+function createChart(schedule, canvasId) {
+    var data = getChartDataFromSchedule(schedule);
+    var ctx = document.getElementById(canvasId);
+    var myChart = new Chart(ctx, data);
+}
+function  createContainer(canvasId) {
+    var scheduleList = document.createElement('div');
+    scheduleList.className = "schedule_list";
+
+    var canvas = document.createElement('canvas');
+    canvas.id = canvasId;
+    var canvasContainer = document.createElement('div');
+    canvasContainer.className = "schedule_pie";
+    canvasContainer.id = "container-" + canvasId;
+
+    canvasContainer.appendChild(canvas);  
+    scheduleList.appendChild(canvasContainer);
+    document.getElementById('wrapper').appendChild(scheduleList);
+
+    canvas = document.createElement('h1');
+    canvas.className = "schedule_title";
+    canvasContainer = document.createTextNode(schedules[canvasId].title + " sleep pattern");
+    canvas.appendChild(canvasContainer);
+
+
+    var text_wrapper = document.createElement('div');
+    text_wrapper.className = "schedule_text";
+
+    text_wrapper.appendChild(canvas);
+
+    canvas = document.createElement('p');
+    canvas.className = "schedule_description";
+    canvasContainer = document.createTextNode(schedules[canvasId].description);
+    canvas.appendChild(canvasContainer);
+
+    text_wrapper.appendChild(canvas);
+    scheduleList.appendChild(text_wrapper);
+
+    canvas = document.createElement('button');
+    canvas.className = "apply_button";
+    canvas.setAttribute('schedule', canvasId);
+    canvas.addEventListener("click", assignScheduleToUser);
+    canvasContainer = document.createTextNode('Apply');
+    canvas.appendChild(canvasContainer);            
+
+    text_wrapper.appendChild(canvas);
+    scheduleList.appendChild(text_wrapper);
+}
+
+function app(user) {
+    for (scheduleId in schedules) {
+        createContainer(scheduleId);
+        createChart(schedules[scheduleId], scheduleId);
+    }
+}
+
+function assignScheduleToUser(event) {
+    console.log(event.target.getAttribute('schedule'));
+    firebase.database().ref('schedule_settings/' + currentUser.uid).set({
+        schedule: event.target.getAttribute('schedule')
+    }).then(function(res) {
+        window.location = '../monitoring/my-schedule.html';
+    });
+}
--- js/table_script.js
+++ js/table_script.js
@@ -1,21 +1,18 @@
-
-
-function edit_row(no)
-{
+function edit_row(no) {
 	document.getElementById("edit_button"+no).style.display="none";
 	document.getElementById("save_button"+no).style.display="inline-block";
 
-	var name=document.getElementById("name_row"+no);
-	var country=document.getElementById("country_row"+no);
-	var age=document.getElementById("age_row"+no);
+	var date=document.getElementById("date_row"+no);
+	var start=document.getElementById("start_row"+no);
+	var stop=document.getElementById("stop_row"+no);
 
-	var name_data=name.innerHTML;
-	var country_data=country.innerHTML;
-	var age_data=age.innerHTML;
+	var date_data=date.innerHTML;
+	var start_data=start.innerHTML;
+	var stop_data=stop.innerHTML;
 
-	name.innerHTML="<input type='text' id='name_text"+no+"' class='flatpickr-data' value='"+name_data+"'>";
-	country.innerHTML="<input type='text' id='country_text"+no+"' class='flatpickr-ora' value='"+country_data+"'>";
-	age.innerHTML="<input type='text' id='age_text"+no+"' class='flatpickr-ora' value='"+age_data+"'>";
+	date.innerHTML="<input type='text' id='date_text"+no+"' class='flatpickr-data table-input' value='"+date_data+"'>";
+	start.innerHTML="<input type='text' id='start_text"+no+"' class='flatpickr-ora table-input' value='"+start_data+"'>";
+	stop.innerHTML="<input type='text' id='stop_text"+no+"' class='flatpickr-ora table-input' value='"+stop_data+"'>";
 
 	flatpickr('.flatpickr-data', {
 		defaultDate: moment().format("YYYY-MM-DD")
@@ -32,32 +29,32 @@ function edit_row(no)
 	});
 }
 
-function save_row(no)
-{
-	var name_val=document.getElementById("name_text"+no).value;
-	var country_val=document.getElementById("country_text"+no).value;
-	var age_val=document.getElementById("age_text"+no).value;
-	document.getElementById("name_row"+no).innerHTML=name_val;
-	document.getElementById("country_row"+no).innerHTML=country_val;
-	document.getElementById("age_row"+no).innerHTML=age_val;
+function save_row(no) {
+	var date_val=document.getElementById("date_text"+no).value;
+	var start_val=document.getElementById("start_text"+no).value;
+	var stop_val=document.getElementById("stop_text"+no).value;
+	document.getElementById("date_row"+no).innerHTML=date_val;
+	document.getElementById("start_row"+no).innerHTML=start_val;
+	document.getElementById("stop_row"+no).innerHTML=stop_val;
 	document.getElementById("save_button"+no).style.display="none";
 	document.getElementById("edit_button"+no).style.display="inline-block";
 }
 
-function delete_row(no)
-{
+function delete_row(no) {
 	document.getElementById("row"+no+"").outerHTML="";
 }
 
-function add_row()
-{
-	var new_name=document.getElementById("calendar").value;
-	var new_country=document.getElementById("start-nap").value;
-	var new_age=document.getElementById("stop-nap").value;
+function add_row() {
+	var new_date=document.getElementById("calendar").value;
+	var new_start=document.getElementById("start-nap").value;
+	var new_stop=document.getElementById("stop-nap").value;
 
 	var table=document.getElementById("data_table");
 	var table_len=(table.rows.length)-1;
-	var row = table.insertRow(table_len).outerHTML="<tr id='row"+table_len+"'><td id='name_row"+table_len+"'>"+new_name+"</td><td id='country_row"+table_len+"'>"+new_country+"</td><td id='age_row"+table_len+"'>"+new_age+"</td><td><button type='button' id='edit_button"+table_len+"' class='edit' onclick='edit_row("+table_len+")'>Edit</button> <button type='button' id='save_button"+table_len+"' class='save' style='display: none' onclick='save_row("+table_len+")'>Save</button> <button type='button' class='delete' onclick='delete_row("+table_len+")'>Delete</button></td></tr>";
-
+	var row = table.insertRow(table_len).outerHTML=
+	"<tr id='row"+table_len+
+	"'><td id='date_row"+table_len+"' class='table-cell'>"+new_date+
+	"</td><td id='start_row"+table_len+"' class='table-cell'>"+new_start+
+	"</td><td id='stop_row"+table_len+"' class='table-cell'>"+new_stop+
+	"</td><td class='v-align'><button type='button' id='edit_button"+table_len+"' class='glyphter edit button-table' onclick='edit_row("+table_len+")'>G</button> <button type='button' id='save_button"+table_len+"' class='glyphter save button-table' style='display: none' onclick='save_row("+table_len+")'>H</button> <button type='button' class='button-table glyphter delete' onclick='delete_row("+table_len+")'>I</button></td></tr>";
 }
-
