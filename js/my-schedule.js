4c3d614ca059ab0567cc7b4e64e24032563b3e5e
Cristian Cojocaru <cristiancojocaru@Cristians-Mac-mini.local>
2017-06-27 09:54:02 +0300

    Login on every page. bug fix on my-schedule

--- js/my-schedule.js
+++
@@ -1,9 +0,0 @@
-flatpickr('.flatpickr-ora', {
-        enableTime: true,
-        noCalendar: true,
-        time_24hr: true,
-        dateFormat: "H:i",
-        defaultDate: "00:00", 
-        defaultHour: 0,
-        defaultMinute: 0
-    });
